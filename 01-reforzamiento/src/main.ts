367b4139cf2be41d3f49fa5e7e3a028f446bcbc2
alemr214 <ale.mr0214@gmail.com>
2026-02-12 20:58:47 -0600

    feat: comment out unused imports in main.ts

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,7 +1,12 @@
 import "./style.css";
 // import "./bases/01-const-let";
 // import "./bases/02-objects";
-import "./bases/03-interfaces";
+// import "./bases/03-interfaces";
+// import "./bases/04-arrays";
+// import "./bases/05-functions";
+// import "./bases/06-obj-destructure";
+// import './bases/07-array-destructure';
+import "./bases/08-work-destructure";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
