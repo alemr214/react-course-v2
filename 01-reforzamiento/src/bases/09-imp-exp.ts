7b4f7dba8f12df7a9b3ef060850d5b50437b7f5d
alemr214 <ale.mr0214@gmail.com>
2026-02-12 22:40:19 -0600

    feat: add console log for getHeroById function to display hero details

--- 01-reforzamiento/src/bases/09-imp-exp.ts
+++ 01-reforzamiento/src/bases/09-imp-exp.ts
@@ -4,3 +4,5 @@ import { heroes, type Hero } from "../data/heroes.data";
 const getHeroById = (id: number): Hero | undefined => {
     return heroes.find((hero) => hero.id === id);
 };
+
+console.log(getHeroById(2));
